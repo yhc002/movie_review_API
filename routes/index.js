8a7494d971092e4b022bf359842ea8a7bba2c0a8
unknown <yhc002@korea.ac.kr>
2020-11-24 15:47:40 +0900

    cors library, swagger host fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
+
 const router = express.Router();
+
+router.use(cors());
 
 router.get('/', function(req, res) {
 	res.send('Root');
